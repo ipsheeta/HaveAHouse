3009d93f04a3e49227dd65c4163f94f0b35b550e
ipsheeta <ipsheeta.furtado@gmail.com>
2018-05-20 12:23:28 -0700

    aligned all images and render order for prezo

---
+++ base.py
@@ -0,0 +1,257 @@
+import os, sys, pygame, os, random
+from pygame.locals import *
+from pygame import constants
+from pgu import gui
+
+from constants import *
+
+def save_file(content, filepath=None):
+    if filepath is None:
+        filepath = os.path.join(BASE_PATH, '.savefiles', 'autosave')
+        chapterKey, page_index = content[0], content[1]
+        to_file = "{} {}".format(chapterKey, page_index)
+        with open(filepath, 'w') as f:
+            f.write(to_file)
+
+
+def load_file(filepath=None):
+    if filepath is None:
+        filepath = os.path.join(BASE_PATH, '.savefiles', 'autosave')
+    with open(filepath, 'r') as f:
+        content = ''
+        for line in f.lines:
+            content += line
+        content = content.split(" ")
+        chapterKey, page_index = content[0], content[1]
+    return str(chapterKey), int(page_index)
+
+
+def load_image(filename, color_key=None):
+    filepath = os.path.join(BASE_PATH, "data", filename)
+    if color_key is None:
+        img = pygame.image.load(filepath).convert()
+        img.set_colorkey(BLACK)
+    else:
+        img = pygame.image.load(filepath).convert_alpha()
+    return img
+
+
+def render_textrect(string, font, rect, text_color, justification=0):
+    final_lines = []
+    requested_lines = string.splitlines()
+    for requested_line in requested_lines:
+        if font.size(requested_line)[0] > rect.width:
+            words = requested_line.split(" ")
+            for word in words:
+                # TODO mad inefficient
+                if font.size(word)[0] >= rect.width:
+                    raise TextRectException("The word exceeds the width of the rectangle")
+
+            accumulated_line = ""
+            for word in words:
+                test_line = accumulated_line + word + " "
+                if font.size(test_line)[0] < rect.width:
+                    accumulated_line = test_line
+                else:
+                    final_lines.append(accumulated_line)
+                    accumulated_line = word + ' '
+            final_lines.append(accumulated_line)
+        else:
+            final_lines.append(requested_line)
+
+    surface = pygame.Surface(rect.size)
+    surface.fill(BLACK)
+    surface.set_colorkey(BLACK)
+
+    accumulated_height = 0
+    for line in final_lines:
+        if accumulated_height + font.size(line)[1] >= rect.height:
+            raise TextRectException("The text vertical height exceeds the height of the rectangle")
+        if line != "":
+            tempsurface = font.render(line, 1, text_color)
+            if justification == 0:
+                surface.blit(tempsurface, (0, accumulated_height))
+            elif justification == 2:
+                surface.blit(tempsurface, (rect.width - tempsurface.get_width(), accumulated_height))
+            else:
+                raise TextRectException("Invalid justification argument")
+            accumulated_height += font.size(line)[1]
+    return surface
+
+
+class TextRectException(Exception):
+    pass
+
+
+class House:
+    value = 0
+    winning = 1000000
+    image = None
+
+class Rural:
+    value = 125000
+    winning = 137500
+    image = 'rural_base_blue.png'
+
+
+class Suburb:
+    value = 400000
+    winning = 440000
+    image = 'rural_base_blue.png'
+
+
+class GameObject:
+    smash = ('smash.png', [390,370], [70,50])
+    leak = ('leak.png', [550,450], [100,170])
+    splat = ('splat.png', [500,375], [100,100])
+    def __init__(self, balance=0, house=House()):
+        self.balance = balance
+        self.house = house
+        self.value = 0
+        self.visualizations = []
+        self.repairs = []
+        self.improvements = [("$10,000 - Install Solar", "install_solar"),
+                             ("$1,000 - Change Paint", "change_paint"),
+                             ("$25,000 - Add Addition", "add_addition"),
+                             ]
+        self.turn = 1
+
+    def spend_money(self, cost):
+        new_balance = self.balance-cost
+        if new_balance < 0:
+            print("Not enough money! Save some first.")
+            raise Exception
+        self.balance = new_balance
+
+    def do_event(self, event):
+        try:
+            method = getattr(self, event)
+            method()
+        except:
+            return
+        mark = None
+        for t in self.improvements:
+            if event == t[1]:
+                mark = t
+        if mark:
+            self.improvements.remove(mark)
+            print(self.improvements)
+            return
+
+        for t in self.repairs:
+            if event == t[1]:
+                mark = t
+
+        if mark:
+            self.repairs.remove(mark)
+            return
+        return
+
+    # improvements
+    def install_solar(self):
+        self.spend_money(10000)
+        self.house.value += 8000
+        self.visualizations.append(("solar-panel.png", [500,275], [75,100]))
+
+    def change_paint(self):
+        self.spend_money(1000)
+        self.house.value *= 1.02
+        self.visualizations.append(("solar-panel.png", [120, 120], [100,100]))
+
+    def add_addition(self):
+        self.spend_money(25000)
+        self.house.value += 20000
+        self.visualizations.append(("addition.png", [285, 393], [150,150]))
+
+    # repairs
+    def fix_leak(self):
+        self.spend_money(10000)
+        self.house.value += 10000
+        self.visualizations.remove(self.leak)
+
+    def fix_smash(self):
+        self.spend_money(500)
+        self.house.value += 500
+        self.visualizations.remove(self.smash)
+
+    def fix_splat(self):
+        self.spend_money(5000)
+        self.house.value += 5000
+        self.visualizations.remove(self.splat)
+
+    def end_turn(self):
+        self.house.value *= 1.01
+        self.balance += 3000
+        if self.turn == 2:
+            self.repairs.append(("$ 10,000 - Fix Leak", "fix_leak"))
+            self.visualizations.append(self.leak)
+            self.house.value *= .60
+        if self.turn == 3:
+            self.repairs.append(("$500 - Fix Smash", "fix_smash"))
+            self.visualizations.append(self.smash)
+            self.house.value *= .80
+        if self.turn == 5:
+            self.repairs.append(("$5,000 - Fix Splat", "fix_splat"))
+            self.visualizations.append(self.splat)
+            self.house.value *= .90
+        self.turn += 1
+
+
+class Scene:
+    font_large = 42
+    font_medium = 30
+    font_small = 24
+    title = ''
+    prompt = ''
+    background = 'background_image.png'
+
+    def __init__(self, gameobject):
+        self.gameobject = gameobject
+        self.choice_list()
+        self.font_l = pygame.font.SysFont(None, self.font_large)
+        self.font_m = pygame.font.SysFont(None, self.font_medium)
+        self.font_s = pygame.font.SysFont(None, self.font_small)
+
+    def choice_list(self):
+        keys = {}
+        choices = self.gameobject.repairs + self.gameobject.improvements
+        self.choices = []
+        for key, (text, event) in enumerate(choices, 1):
+            keys[getattr(constants, "K_{}".format(key))] = event
+            self.choices.append((key, text))
+        self.keys = keys
+
+    def render(self, screen):
+        screen.fill(BLACK)
+        if self.background:
+            image = pygame.image.load(os.path.join(BASE_PATH, 'data', self.background))
+            image = pygame.transform.scale(image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+            screen.blit(image, [0, 0])
+        title = self.font_l.render(self.title, True, WHITE)
+        screen.blit(title, XY_TITLE)
+        prompt = self.font_m.render(self.prompt, True, WHITE)
+        screen.blit(prompt, XY_PROMPT)
+        show_balance = self.font_m.render("$ "+str(self.gameobject.balance), True, GREEN)
+        screen.blit(show_balance, XY_BALANCE)
+        show_value = self.font_m.render("$ "+str(self.gameobject.house.value), True, YELLOW)
+        screen.blit(show_value, XY_VALUE)
+        accumulated_height = Y_CHOICES
+        for choice, text in self.choices:
+            choice = self.font_s.render("> {}. {}".format(choice, text), True, WHITE)
+            screen.blit(choice, (X_CHOICES, accumulated_height))
+            accumulated_height += INC_CHOICES
+
+
+    def handle_events(self, events):
+        for e in events:
+            if e.type == KEYDOWN and e.key in self.keys:
+                # fire event to game object
+                self.gameobject.do_event(self.keys[e.key])
+                return
+            if e.type == KEYDOWN and e.key == K_SPACE:
+                self.gameobject.end_turn()
+                # self.update() #add night background animation
+
+    def update(self):
+        self.choice_list()
+
--- main.py
+++ main.py
@@ -3,264 +3,23 @@ from pygame.locals import *
 from pygame import constants
 from pgu import gui
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-DISPLAY = (SCREEN_WIDTH, SCREEN_HEIGHT)
-FLAGS = 0
-DEPTH = 0
-FPS = 0
-TITLE = 'Have A House'
-
-XY_TITLE = (20, 25)
-XY_PROMPT = (20, 70)
-XY_BALANCE = (700, 70)
-XY_VALUE = (700, 100)
-X_CHOICES = 25
-Y_CHOICES = 120
-INC_CHOICES = 35
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-YELLOW = (255, 255, 0)
-BASE_PATH = os.getcwd()
-
-
-def save_file(content, filepath=None):
-    if filepath is None:
-        filepath = os.path.join(BASE_PATH, '.savefiles', 'autosave')
-        chapterKey, page_index = content[0], content[1]
-        to_file = "{} {}".format(chapterKey, page_index)
-        with open(filepath, 'w') as f:
-            f.write(to_file)
-
-
-def load_file(filepath=None):
-    if filepath is None:
-        filepath = os.path.join(BASE_PATH, '.savefiles', 'autosave')
-    with open(filepath, 'r') as f:
-        content = ''
-        for line in f.lines:
-            content += line
-        content = content.split(" ")
-        chapterKey, page_index = content[0], content[1]
-    return str(chapterKey), int(page_index)
-
-
-def load_image(filename, color_key=None):
-    filepath = os.path.join(BASE_PATH, "data", filename)
-    if color_key is None:
-        img = pygame.image.load(filepath).convert()
-        img.set_colorkey(BLACK)
-    else:
-        img = pygame.image.load(filepath).convert_alpha()
-    return img
-
-
-def render_textrect(string, font, rect, text_color, justification=0):
-    final_lines = []
-    requested_lines = string.splitlines()
-    for requested_line in requested_lines:
-        if font.size(requested_line)[0] > rect.width:
-            words = requested_line.split(" ")
-            for word in words:
-                # TODO mad inefficient
-                if font.size(word)[0] >= rect.width:
-                    raise TextRectException("The word exceeds the width of the rectangle")
-
-            accumulated_line = ""
-            for word in words:
-                test_line = accumulated_line + word + " "
-                if font.size(test_line)[0] < rect.width:
-                    accumulated_line = test_line
-                else:
-                    final_lines.append(accumulated_line)
-                    accumulated_line = word + ' '
-            final_lines.append(accumulated_line)
-        else:
-            final_lines.append(requested_line)
-
-    surface = pygame.Surface(rect.size)
-    surface.fill(BLACK)
-    surface.set_colorkey(BLACK)
-
-    accumulated_height = 0
-    for line in final_lines:
-        if accumulated_height + font.size(line)[1] >= rect.height:
-            raise TextRectException("The text vertical height exceeds the height of the rectangle")
-        if line != "":
-            tempsurface = font.render(line, 1, text_color)
-            if justification == 0:
-                surface.blit(tempsurface, (0, accumulated_height))
-            elif justification == 2:
-                surface.blit(tempsurface, (rect.width - tempsurface.get_width(), accumulated_height))
-            else:
-                raise TextRectException("Invalid justification argument")
-            accumulated_height += font.size(line)[1]
-    return surface
-
-
-class TextRectException(Exception):
-    pass
-
-
-class House:
-    value = 0
-    winning = 1000000
-
-
-class Rural:
-    value = 125000
-    winning = 137500
-
-
-class Suburb:
-    value = 400000
-    winning = 440000
-
-
-class GameObject:
-    def __init__(self, balance=0, house=House()):
-        self.balance = balance
-        self.house = house
-        self.value = 0
-        self.repairs = []
-        self.improvements = [("Install Solar", "install_solar"),
-                             ("Change Paint", "change_paint"),
-                             ("Add Addition", "add_addition"),
-                             ]
-        self.turn = 1
-
-    def spend_money(self, cost):
-        new_balance = self.balance-cost
-        if new_balance < 0:
-            print("Not enough money! Save some first.")
-            return
-        self.balance = new_balance
-
-    def do_event(self, event):
-        method = getattr(self, event)
-        method()
-        mark = None
-        for t in self.improvements:
-            if event == t[1]:
-                mark = t
-        if mark:
-            self.improvements.remove(mark)
-            print(self.improvements)
-            return
-
-        for t in self.repairs:
-            if event == t[1]:
-                mark = t
-
-        if mark:
-            self.repairs.remove(mark)
-            return
-        return
-
-    # improvements
-    def install_solar(self):
-        self.spend_money(10000)
-        self.house.value += 8000
-
-    def change_paint(self):
-        self.spend_money(1000)
-        self.house.value *= 1.02
-
-    def add_addition(self):
-        self.spend_money(25000)
-        self.house.value += 20000
-
-    # repairs
-    def fix_leak(self):
-        self.spend_money(10000)
-        self.house.value += 10000
-
-    def fix_smash(self):
-        self.spend_money(500)
-        self.house.value += 500
-
-    def fix_splat(self):
-        self.spend_money(5000)
-        self.house.value += 5000
-
-    def end_turn(self):
-        self.house.value *= 1.01
-        self.balance += 3000
-        if self.turn == 2:
-            self.repairs.append(("Fix Leak", "fix_leak"))
-            self.house.value *= .60
-        if self.turn == 3:
-            self.repairs.append(("Fix Smash", "fix_smash"))
-            self.house.value *= .80
-        if self.turn == 5:
-            self.repairs.append(("Fix Splat", "fix_splat"))
-            self.house.value *= .90
-        self.turn += 1
-
-
-class Scene:
-    font_large = 42
-    font_medium = 30
-    font_small = 24
-    title = ''
-    prompt = ''
-    background = None
-
-    def __init__(self, gameobject):
-        self.gameobject = gameobject
-        self.choice_list()
-        self.font_l = pygame.font.SysFont(None, self.font_large)
-        self.font_m = pygame.font.SysFont(None, self.font_medium)
-        self.font_s = pygame.font.SysFont(None, self.font_small)
-
-    def choice_list(self):
-        keys = {}
-        choices = self.gameobject.repairs + self.gameobject.improvements
-        self.choices = []
-        for key, (text, event) in enumerate(choices, 1):
-            keys[getattr(constants, "K_{}".format(key))] = event
-            self.choices.append((key, text))
-        self.keys = keys
-
-    def render(self, screen):
-        screen.fill(BLACK)
-        if self.background:
-            image = pygame.image.load(os.path.join(BASE_PATH, 'data', self.background))
-            image = pygame.transform.scale(image, (SCREEN_WIDTH, SCREEN_HEIGHT))
-            screen.blit(image, [0, 0])
-        title = self.font_l.render(self.title, True, WHITE)
-        screen.blit(title, XY_TITLE)
-        prompt = self.font_m.render(self.prompt, True, WHITE)
-        screen.blit(prompt, XY_PROMPT)
-        show_balance = self.font_m.render("$ "+str(self.gameobject.balance), True, GREEN)
-        screen.blit(show_balance, XY_BALANCE)
-        show_value = self.font_m.render("$ "+str(self.gameobject.house.value), True, YELLOW)
-        screen.blit(show_value, XY_VALUE)
-        accumulated_height = Y_CHOICES
-        for choice, text in self.choices:
-            choice = self.font_s.render("> {}. {}".format(choice, text), True, WHITE)
-            screen.blit(choice, (X_CHOICES, accumulated_height))
-            accumulated_height += INC_CHOICES
-
-    def handle_events(self, events):
-        for e in events:
-            if e.type == KEYDOWN and e.key in self.keys:
-                # fire event to game object
-                self.gameobject.do_event(self.keys[e.key])
-                return
-            if e.type == KEYDOWN and e.key == K_SPACE:
-                self.gameobject.end_turn()
-                # self.update() #add night background animation
-
-    def update(self):
-        self.choice_list()
-
+from constants import *
+from base import Scene, Rural, Suburb, GameObject
 
 class TestScene(Scene):
-    title = "Test"
-    prompt = "Prompt Test"
+    title = "Having a House"
+    prompt = "How would you like to care for your home?"
+
+    def render(self, screen):
+        super().render(screen)
+        house_img = pygame.image.load(os.path.join(BASE_PATH, 'data', 'rural_base_blue.png'))
+        house_img = pygame.transform.scale(house_img, (200, 250))
+        screen.blit(house_img, [400,300])
+        for image, coords, scale in self.gameobject.visualizations:
+            image = pygame.image.load(os.path.join(BASE_PATH, 'data', image))
+            image = pygame.transform.scale(image, scale)
+            screen.blit(image, coords)
+
 
 class TitleScene(Scene):
     def __init__(self, gameobject):
@@ -272,7 +31,7 @@ class TitleScene(Scene):
         self.instructions = [
             self.font_s.render("> Press 'Space Bar' for Start Game", True, WHITE),
             self.font_s.render("> Press 'Escape' to Exit", True, WHITE),
-            self.font_s.render("> Press 'O' to Load Game ", True, WHITE),
+            # self.font_s.render("> Press 'O' to Load Game ", True, WHITE),
             # self.font_s.render("> Press 'I' for instructions", True, WHITE)
         ]
         super().__init__(gameobject)
@@ -284,9 +43,6 @@ class TitleScene(Scene):
         for e in events:
             if e.type == KEYDOWN and e.key == K_ESCAPE:
                 terminate()
-            # if e.type == KEYDOWN and e.key == K_o:
-            #     chapterKey, page_index = load_file()
-            #     self.manager.go_to(StoryScene(chapterKey, page_index))
             if e.type == KEYDOWN and e.key == K_SPACE:
                 self.manager.go_to(StartScene(self.gameobject))
 
@@ -342,33 +98,6 @@ class StartScene(Scene):
             if e.type == KEYDOWN and e.key == K_b:
                 self.gameobject.house = Rural()
                 self.manager.go_to(TestScene(self.gameobject))
-
-
-class InstructionScene(Scene):
-    def __init__(self):
-        self.font_l = pygame.font.SysFont(None, 32)
-        self.font_s = pygame.font.SysFont(None, 18)
-        self.title = self.font_l.render("Instructions", True, WHITE)
-        self.instructions = [
-            self.font_s.render("> ", True, WHITE),
-            self.font_s.render("> ", True, WHITE)
-        ]
-
-    def update(self):
-        pass
-
-    def render(self, screen):
-        screen.fill(BLACK)
-        screen.blit(self.title, (45, 25))
-        instruction_height = 100
-        for instruction in self.instructions:
-            screen.blit(instruction, (45, instruction_height))
-            instruction_height += 25
-
-    def handle_events(self, events):
-        for e in events:
-            if e.type == KEYDOWN and e.key == K_ESCAPE:
-                self.manager.go_to(TitleScene())
 
 
 class SceneManager:
